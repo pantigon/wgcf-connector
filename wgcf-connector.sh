05d93c4bb8214b739cb1b04cec107f8d2f3721aa
Anim Mouse <git@animmouse.com>
2025-09-01 07:40:06 +0800

    fix: Use comma-separated values on WireGuard configuration and add more DNS servers

--- wgcf-connector.sh
+++ wgcf-connector.sh
@@ -8,16 +8,13 @@ cat > /app/output/wgcf-connector-$(jq -r .registration_id[0] < /var/lib/cloudfla
 # Organization: $(jq -r .account.organization < /var/lib/cloudflare-warp/conf.json)
 [Interface]
 PrivateKey = $(jq -r .secret_key < /var/lib/cloudflare-warp/reg.json)
-Address = $(jq -r .interface.v6 < /var/lib/cloudflare-warp/conf.json)/64
-Address = $(jq -r .interface.v4 < /var/lib/cloudflare-warp/conf.json)/12
-DNS = 2606:4700:4700::1111
-DNS = 1.1.1.1
+Address = $(jq -r .interface.v6 < /var/lib/cloudflare-warp/conf.json)/64, $(jq -r .interface.v4 < /var/lib/cloudflare-warp/conf.json)/12
+DNS = 2606:4700:4700::1111, 2606:4700:4700::1001, 1.1.1.1, 1.0.0.1
 MTU = 1420
 
 [Peer]
 PublicKey = $(jq -r .public_key < /var/lib/cloudflare-warp/conf.json)
-AllowedIPs = ::/0
-AllowedIPs = 0.0.0.0/0
+AllowedIPs = ::/0, 0.0.0.0/0
 Endpoint = $(jq -r .endpoints[0].v4 < /var/lib/cloudflare-warp/conf.json)
 #Endpoint = $(jq -r .endpoints[0].v6 < /var/lib/cloudflare-warp/conf.json)
 
